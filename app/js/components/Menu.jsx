214185018d7e076cef2093b4a826fb4c4a3c165d
Henry Netzer <hnetzer19@gmail.com>
2017-03-09 11:52:17 -0800

    workings begingings of webpack stuff

---
+++ app/js/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Menu extends Component {
+  render() {
+    return (
+      <div className='Menu'>
+        Whatever the hell is going on here.
+      </div>
+    );
+  }
+}
